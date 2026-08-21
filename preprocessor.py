54f927228daa1941cdb3bb870116a507ae6898d7
ShraddhaR11 <shraddha9042@gmail.com>
2026-08-21 16:06:37 +0530

    first commit

---
+++ preprocessor.py
@@ -0,0 +1,71 @@
+import re
+import pandas as pd
+
+def preprocess(data):
+    pattern = r'\[\d{2}/\d{2}/\d{2},\s*\d{1,2}:\d{2}:\d{2}\s*[AP]M\]\s*'
+    messages = re.split(pattern,data)
+    dates = re.findall(pattern, data)
+    messages = messages[:len(dates)]
+    dates = [
+    date.replace('[', '').replace(']', '').replace('\u202f', ' ').strip()
+    for date in dates
+]
+
+    df = pd.DataFrame({'user_message':messages,'message_date':dates})
+    #convert message_date type
+    df['message_date'] = pd.to_datetime(
+        df['message_date'],
+        format='%d/%m/%y, %I:%M:%S %p'
+    )
+
+    # Rename column
+    df.rename(
+        columns={'message_date': 'date'},
+        inplace=True
+    )
+
+
+    users = []
+    messages = []
+
+    for message in df['user_message']:
+
+        # If message is a list, convert it to string
+        if isinstance(message, list):
+            message = ' '.join(message)
+
+        entry = re.split(r'^([^:]+):\s*', message, maxsplit=1)
+
+        if len(entry) > 2:
+            users.append(entry[1].strip())
+            messages.append(entry[2].strip())
+        else:
+            users.append('group_notification')
+            messages.append(entry[0].strip())
+
+    df['user'] = users
+    df['message'] = messages
+
+    df.drop(columns=['user_message'], inplace=True)
+
+    df['only_date'] = df['date'].dt.date
+    df['year'] = df['date'].dt.year
+    df['month_num'] = df['date'].dt.month
+    df['month'] = df['date'].dt.month_name()
+    df['day'] = df['date'].dt.day
+    df['day_name'] = df['date'].dt.day_name()
+    df['hour'] = df['date'].dt.hour
+    df['minute'] = df['date'].dt.minute
+
+    period = []
+    for hour in df[['day_name', 'hour']]['hour']:
+        if hour == 23:
+            period.append(str(hour) + "-" + str('00'))
+        elif hour == 0:
+            period.append(str('00') + "-" + str(hour + 1))
+        else:
+            period.append(str(hour) + "-" + str(hour + 1))
+
+    df['period'] = period
+
+    return df
